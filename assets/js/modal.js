388a9e1b8789e8ffdf050bf2da1314bd09981d1d
RHoganD <richpatric@hotmail.com>
2022-11-04 00:05:50 +0000

    Ajust CSS and add js code

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -9,23 +9,13 @@ let span = document.getElementsByClassName("close")[0];
 
 // When clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = "flex";
 }
 
 // When  clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
-// When clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-let w = 480, h = 340;
 
 
 function openWindow(){
@@ -34,19 +24,19 @@ if (document.getElementById) {
    h = screen.availHeight;
 }  
 
-let popW = 650, popH = 650;
+let popW = 540, popH = 540;
 
-let leftPos = (w-popW)/2;
-let topPos = (h-popH)/2;
+let leftPos = (w-popW)/3;
+let topPos = (h-popH)/3;
 
 
 
-msgWindow = window.open('','popup','width=' + popW + ',height=' + popH + 
-                         ',top=' + topPos + ',left=' + leftPos + ',       scrollbars=yes');
+gsWindow = window.open('','popup','width=' + popW + ',height=' + popH + 
+                         ',top=' + topPos + ',left=' + leftPos + ', ');
 
-msgWindow.document.write 
+gsWindow.document.write 
     ('<HTML><HEAD><TITLE>Reveal Puzzle</TITLE></HEAD><BODY><FORM  NAME="form1">' +
-    '<img src="./assets/images/elsa_and_anna_frozen.jpg"/>'+
+    '<img src="./assets/images/elsa_and_anna_frozen.jpg" alt="frozen" class="responsive"/>'+
     ' <H1>Completed Puzzle Image.</H1>' + ' Click the button below to close the window.<br />' +
     '<INPUT TYPE="button" VALUE="OK"onClick="window.close();"></FORM></BODY>   </HTML>');
 }
---
+++ assets/js/puzzle.js
@@ -0,0 +1,9 @@
+//variables
+
+const rows = 3;
+const columns = 5;
+
+let currentTile;
+let otherTile;  
+
+let turns = 0;
